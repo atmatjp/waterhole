1b4486aa795c375a8e02663b815f22115ea6989e
atmatjp <keigo21emon@gmail.com>
2025-05-02 01:48:22 +0900

    update

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -14,7 +14,7 @@
     </button>
     <nav class:open={menuOpen}>
       <a href="/about">about</a>
-      <a href="#">github</a>
+      <a href="https://github.com/atmatjp/butumori">github</a>
     </nav>
   </div>
 </header>
