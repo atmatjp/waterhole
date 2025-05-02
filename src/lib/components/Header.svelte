7974a1e2b6b220c06a7d603f1f5e020ffb800723
atmatjp <keigo21emon@gmail.com>
2025-05-03 00:00:51 +0900

    update

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,17 +3,30 @@
   import "../../app.css";
 
   let menuOpen = false;
+  let menuRef: HTMLDivElement;
+
+  onMount(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuOpen && menuRef && !menuRef.contains(event.target as Node)) {
+        menuOpen = false;
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  });
 </script>
 
 <header>
-  <div class="header-container">
-    <h1><a href="/">大阪万博自販機マップβ</a></h1>
-    <button class="hamburger" on:click={() => (menuOpen = !menuOpen)}>
-      ☰
-    </button>
-    <nav class:open={menuOpen}>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-    </nav>
+  <div bind:this={menuRef}>
+    <div class="header-container">
+      <h1><a href="/">大阪万博自販機マップβ</a></h1>
+      <button class="hamburger" on:click={() => (menuOpen = !menuOpen)}>
+        ☰
+      </button>
+      <nav class:open={menuOpen}>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+      </nav>
+    </div>
   </div>
 </header>
